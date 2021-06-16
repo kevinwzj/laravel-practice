4e12a72606871cd6fe1d33e355d290325fb92885
Kevin Wu <wuzejun@gmail.com>
2021-06-16 14:51:06 -0700

    add test for create user

--- tests/Feature/Http/Controllers/LeaderControllerTest.php
+++ tests/Feature/Http/Controllers/LeaderControllerTest.php
@@ -2,34 +2,36 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LeaderControllerTest extends TestCase
 {
-    protected $leader;
+    use DatabaseTransactions;
 
-    public function setUp() :void{
-        parent::setUp();
-        $this->leader = $this->app->make('App\Http\Controllers\LeaderController');
+    public function testCreateUserSuccessAction()
+    {
+        $requestParams = [
+            'name'      => 'Nancy',
+            'age'       => 31,
+            'address'   => '1787 156 ST Surrey BC V3A 6S9'
+        ];
+        $this->post('/api/user/leaderboard', $requestParams)
+            ->assertJson([
+                'message' => 'Leader ' . $requestParams['name'] . ' has been created.'
+            ]);
     }
 
     public function testCreateUserFailAction()
     {
         $paramMissing = 'age';
         $requestParams = [
-            'name'      => 'Kevin',
+            'name'      => 'Nancy',
             'address'   => '1787 156 ST Surrey BC V3A 6S9'
         ];
-        $request = new Request($requestParams);
-        $result = $this->leader->create($request);
-        $result = response()->json($result)->getContent();
-        $result = json_decode($result, true);
-        $result = $result['original'];
-        $actual = $result['message'];
-        $this->assertTrue($actual == 'Creation failed, ' . $paramMissing . ' is a required field.');
+        $this->post('/api/user/leaderboard', $requestParams)
+            ->assertJson([
+                'message' => 'Creation failed, ' . $paramMissing . ' is a required field.'
+            ]);
     }
 }
